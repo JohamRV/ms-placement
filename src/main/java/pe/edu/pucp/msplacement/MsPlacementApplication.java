dc7c32dd9796e2ccb69ce54f30dcd3f9703ac433
Melanie Espinoza <melanie.espinoza@pucp.edu.pe>
2023-07-09 02:08:27 -0500

    feat: set up placement project

---
+++ src/main/java/pe/edu/pucp/msplacement/MsPlacementApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.msplacement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPlacementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPlacementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/pucp/msplacement/MsPlacementApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.msplacement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPlacementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
